3efd5807e250fb4163687dc3321fcb4422af696d
rylegames <rylee.nyc@gmail.com>
2023-10-16 10:46:35 -0400

    columns

--- src/lib/Columns.svelte
+++ src/lib/Columns.svelte
@@ -7,7 +7,24 @@
 	export let block: Block;
 </script>
 
-<div id={block.id} class={twMerge('mb-2 grid grid-flow-col gap-2', clazz)}>
+<div
+	id={block.id}
+	class={twMerge(
+		'mb-2 grid grid-flow-row gap-4',
+		block.children.length === 5
+			? 'grid-cols-5'
+			: block.children.length === 4
+			? 'grid-cols-4'
+			: block.children.length === 3
+			? 'grid-cols-3'
+			: block.children.length === 2
+			? 'grid-cols-2'
+			: block.children.length === 1
+			? 'grid-cols-1'
+			: 'grid-flow-column',
+		clazz
+	)}
+>
 	{#each block.children || [] as column (column.id)}
 		<div>
 			<slot {column} />
